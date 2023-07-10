9bcf18e0b39e82a9f5e3df79d01cd37a636d03af
Rhydham <100224159+Rhydham2809@users.noreply.github.com>
2023-07-10 21:06:33 +0530

    Add files via upload

---
+++ Lab-5/Lab5P2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Area
+{
+    public double Area(double x)
+    {
+        return x * x;
+    }
+
+    public double Area(double l, double b)
+    {
+        return l * b;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Area area = new Area();
+        Console.WriteLine(area.Area(10));
+        Console.WriteLine(area.Area(3, 4));
+    }
+}
+
